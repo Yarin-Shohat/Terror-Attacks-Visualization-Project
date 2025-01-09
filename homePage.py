29a25d70545354fb3d9e251e462782dfdc88c0cb
Yarin Shohat <63013402+Yarin-Shohat@users.noreply.github.com>
2025-01-09 20:24:38 +0000

    update home page

--- homePage.py
+++ homePage.py
@@ -17,6 +17,21 @@ st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 # -----------------------------------------------------------------------------
 # Draw the actual page
+@st.cache_data
+def get_data():
+    """
+    Read the Terror Attacks data from the CSV file.
+    """
+
+    # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
+    DATA_FILENAME = Path(__file__).parent/'data/IL_data.csv'
+    df = pd.read_csv(DATA_FILENAME)
+    columns_names = ["eventid","iyear","imonth","iday","country","city","latitude","longitude","nperps","nkill","nwound",
+        "location","success","attacktype1","suicide","targtype1","weaptype1_txt","gname","extended"]
+
+    df = df[columns_names]
+
+    return df
 
 
 # Set the title that appears at the top of the page.
@@ -24,13 +39,31 @@ st.markdown(
     '''
     # :boom: Terror Attacks Visualization Project
     <div style="text-align: right; direction: rtl;">
-
-    בחרנו לעבד, לנתח ולהציג נתונים אודות אירועי טרור בישראל לאורך השנים, מתוך מאגר הנתונים Global Terrorism Database (GTD).
+    פרויקט בקורס ויזואליזציה במטרה לעבד, לנתח ולהציג נתונים אודות אירועי טרור בישראל לאורך השנים, מתוך מאגר הנתונים Global Terrorism Database (GTD).
+    <br>
+    בפרויקט התמקדנו בפיגועים שהתרחשו בשטח ישראל בלבד, במטרה להבין את דפוסי הפעולה והמאפיינים הייחודיים למרחב המקומי.
+    <br><br>
+    השאלה המרכזית עליה נרצה לענות בעזרת הנתונים שבחרנו היא:
+    <br>
+    <span><b>כיצד השתנה דפוס הטרור בישראל לאורך השנים מבחינת תדירות האירועים, הפיזור הגיאוגרפי ושיטות הפעולה?</b></span>
+    <br>
+    מטרתנו בפרויקט היא להבין את מגמות הטרור בישראל לאורך זמן, תוך התמקדות בזיהוי מוקדי הפעולה המרכזיים, קשרים בין מאפייני האירועים והתפתחותם ההיסטורית.
+    <br><br>
+    יתר על כן, נרצה לענות על מספר שאלות משנה נוספות:
     </div>
     ''',
     unsafe_allow_html=True
 )
-
+st.markdown(
+        '''
+        <ol style="text-align: right; direction: rtl;">
+            <li>תדירות התקיפות לפי אזורים: מהם האזורים בהם התרחשו המספר הגבוה ביותר של תקיפות טרור?</li>
+            <li>קורלציה בין מספר המחבלים למספר הנפגעים: האם קיים קשר בין כמות המחבלים באירוע לכמות הנפגעים?</li>
+            <li>כמות פיגועי הטרור לאורך השנים: כיצד השתנתה תדירות הפיגועים לאורך השנים?</li>
+        </ol>
+        ''',
+        unsafe_allow_html=True
+)
 # Add some spacing
 ''
 ''
