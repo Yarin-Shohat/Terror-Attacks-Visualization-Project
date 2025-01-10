abd7d32b6967d270d92683e2f1255316459f3d8b
Yarin Shohat <63013402+Yarin-Shohat@users.noreply.github.com>
2025-01-10 11:45:21 +0000

    update homepage

--- homePage.py
+++ homePage.py
@@ -21,7 +21,7 @@ def get_data():
 
     # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
     DATA_FILENAME = Path(__file__).parent/'data/IL_data.csv'
-    df = pd.read_csv(DATA_FILENAME)
+    df = pd.read_csv(DATA_FILENAME, encoding='ISO-8859-1')
     columns_names = ["eventid","iyear","imonth","iday","country","city","latitude","longitude","nperps","nkill","nwound",
         "location","success","attacktype1","suicide","targtype1","weaptype1_txt","gname","extended"]
 
@@ -64,3 +64,5 @@ st.markdown(
 ''
 ''
 
+df = get_data()
+st.write(df)
